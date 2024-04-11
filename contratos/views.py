90a184105f589b38aa0addbf8ab3b3e273d7f2c0
1Pampu <piampianimartin@gmail.com>
2024-04-11 18:33:48 -0300

    style errors

--- contratos/views.py
+++ contratos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Contrato, Inmueble, Persona
+from .models import Contrato, Inmueble
 from .forms import ContratoForm
 from .utils import autocompletar_docx, numero_a_texto, fecha_a_texto
 from django.utils import timezone
